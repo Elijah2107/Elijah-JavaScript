5b1d8d14b94b83eec8c605b58cbe40f96f151a9e
Elijah2107 <olgawong@hotmail.com>
2025-07-28 10:58:11 +0000

    Jul 28, 2025, 10:58 AM

--- L04_mouseclick.js
+++ L04_mouseclick.js
@@ -23,9 +23,9 @@ function draw() {
 }
 
 function mousePressed() {
-    
+    shapeColor = color(random(255), random(255), random(255));
 }
 
 function mouseReleased() {
-    shapeColor = color(random(255), random(255), random(255));
+    
 }
