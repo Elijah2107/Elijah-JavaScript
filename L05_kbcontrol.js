618a09bdfa0e741180d4d03d0a1f66ce6c3be0d9
Elijah2107 <olgawong@hotmail.com>
2025-08-04 10:07:34 +0000

    Aug 4, 2025, 10:07 AM

--- L05_kbcontrol.js
+++ L05_kbcontrol.js
@@ -1,5 +1,5 @@
-let circleSize = 5
-let shape
+let circleSize = 5;
+let shapeColor = "blue";
 function setup() {
     createCanvas(600,400)
     background(220)
@@ -10,9 +10,9 @@ function draw() {
 }
 
 function mousePressed() {
-    shapeColor = color(random(255), random(255) , random(255))
-    fill(shapeColor)
-    size = 5
+    shapeColor = color(random(255), random(255) , random(255));
+    fill(shapeColor);
+    circleSize = 5;
 }
 function mouseDragged() {
     circle(mouseX, mouseY, size);
