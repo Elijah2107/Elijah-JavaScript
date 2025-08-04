fef4cc14516d9294769442c66b2d096afd2f7869
Elijah2107 <olgawong@hotmail.com>
2025-08-04 10:06:04 +0000

    Aug 4, 2025, 10:06 AM

--- L05_kbcontrol.js
+++ L05_kbcontrol.js
@@ -10,9 +10,8 @@ function draw() {
 function mousePressed() {
     shapeColor = color(random(255), random(255) , random(255))
     fill(shapeColor)
-    
+    size = 5
 }
-let size = 5
 function mouseDragged() {
     circle(mouseX, mouseY, size);
     let size = size + 0.5
