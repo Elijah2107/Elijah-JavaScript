e60a393610f77ffb4826840be80edbc5f2a48ef2
Elijah2107 <olgawong@hotmail.com>
2025-08-11 11:00:45 +0000

    Aug 11, 2025, 11:00 AM

--- L06_spritesoundfx.js
+++ L06_spritesoundfx.js
@@ -2,6 +2,7 @@ let soundEffect, bgMusic, staticImage;
 function preload() {
     soundEffect = loadSouns("assets/pop.mp3");
     bgMusic = loadSouns("assets/bossaNova.mp3");
-    staticImage = loadImage("assets/pico-a")
+    staticImage = loadImage("assets/pico-a.png");
+
 }
 
