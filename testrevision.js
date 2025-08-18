2d6dc34e1e5d9b85e13d3c22b6f6288da88173e5
Elijah2107 <olgawong@hotmail.com>
2025-08-18 10:08:34 +0000

    Aug 18, 2025, 10:08 AM

--- testrevision.js
+++ testrevision.js
@@ -1,17 +1,13 @@
 let x = 100;
 let y = 100;
 let w = 30;
-let h - 30
+let h = 30;
+let gray = 0;
 function setup() {
     createcanvas(800,600);
     background("steelblue")
     noLoop();
 }
-
-
-
-
-
 function draw() {
     fill("red")
     //triangle(350,300,450,300,400,200);
