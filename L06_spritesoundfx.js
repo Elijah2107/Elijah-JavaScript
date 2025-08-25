37de2d92bb7e83e7745f3c92e81d59826bf4676a
Elijah2107 <olgawong@hotmail.com>
2025-08-25 10:41:51 +0000

    Aug 25, 2025, 10:41 AM

--- L06_spritesoundfx.js
+++ L06_spritesoundfx.js
@@ -3,7 +3,7 @@ let x =0;
 let y = 0;
 function preload() {
     soundEffect = loadSound("assets/pop.mp3");
-    bgMusic = loadSound("assets/star_ward_theme_8");
+    bgMusic = loadSound("assets/star_ward_theme_8_bit.mp3");
     staticImage = loadImage("assets/pico-a.png");
 }
 
