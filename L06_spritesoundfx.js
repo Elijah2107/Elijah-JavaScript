c94b13971b4ce7d203a5fac224100631ae3c0a5a
Elijah2107 <olgawong@hotmail.com>
2025-08-25 10:55:39 +0000

    Aug 25, 2025, 10:55 AM

--- L06_spritesoundfx.js
+++ L06_spritesoundfx.js
@@ -20,7 +20,7 @@ function draw() {
     let w = 110;
     let h = 123;
     image(staticImage, x,y,w,h);
-    image(secondGuy,x,y,w,h)
+    image(secondGuy,200,,w,h)
     if (keyIsDown( LEFT_ARROW)) {
         x = x-5;
     }
