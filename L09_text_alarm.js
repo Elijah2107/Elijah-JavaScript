e9baffa71fb8add4579515a8d555c8c22e946a28
Elijah2107 <olgawong@hotmail.com>
2025-09-22 09:41:33 +0000

    Sep 22, 2025, 9:41 AM

--- L09_text_alarm.js
+++ L09_text_alarm.js
@@ -1,10 +1,9 @@
 function setup() {
     createCanvas(600,400);
-    background(0);
+    background(220);
 }
 function draw() {
     translate(width/2,ypos);
-    
     textSize(75);
     textAlign(CENTER,CENTER);
 }
