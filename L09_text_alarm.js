ed926d328f72d52562489aad49c89d258b965980
Elijah2107 <olgawong@hotmail.com>
2025-09-22 10:04:15 +0000

    Sep 22, 2025, 10:04 AM

--- L09_text_alarm.js
+++ L09_text_alarm.js
@@ -5,16 +5,17 @@ function setup() {
     background(220);
     xpos = width/2;
 }
-function draw() {
-    background(220);
-    textSize(32);
-    textAlign(CENTER,CENTER);
-    text("Bounce!", xpos, height/2);
-    xpos = xpos + velocityX;
-    if (xpos > width) {
-        velocityX = velocityX *-1;
-    }
-    else if (xpos < 0) {
-        velocityX = velocityX *-1;
-    }
-}
+function draw()
+// function draw() {
+//     background(220);
+//     textSize(32);
+//     textAlign(CENTER,CENTER);
+//     text("Bounce!", xpos, height/2);
+//     xpos = xpos + velocityX;
+//     if (xpos > width) {
+//         velocityX = velocityX *-1;
+//     }
+//     else if (xpos < 0) {
+//         velocityX = velocityX *-1;
+//     }
+// }
