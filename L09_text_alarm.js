64f3c5147f16eb21186d6a67c8d5571fbedee7b9
Elijah2107 <olgawong@hotmail.com>
2025-09-22 10:23:50 +0000

    Sep 22, 2025, 10:23 AM

--- L09_text_alarm.js
+++ L09_text_alarm.js
@@ -12,6 +12,7 @@ function draw() {
     let aMinute = minute();
     aMinute = nf(aMinute, 2);
     let aSecond = second();
+    aSecond
     textSize(32);
     textAlign(CENTER,CENTER);
     fill("red");
