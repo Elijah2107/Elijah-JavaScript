734774279cd0b3261c8a26836c106752daec45c5
Elijah2107 <olgawong@hotmail.com>
2025-09-29 10:58:00 +0000

    Sep 29, 2025, 10:58 AM

--- L10_name.js
+++ L10_name.js
@@ -7,7 +7,7 @@ function setup() {
     colorPicker.position(width/2, height/2);
 }
 function draw() {
-    background (colorPicker.value)
+    background (colorPicker.value());
     fill("white");
     rect(100,50,400,100,25);
 }
