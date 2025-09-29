1468c51e160229659c8bcff021ed202d6677cbf9
Elijah2107 <olgawong@hotmail.com>
2025-09-29 10:10:00 +0000

    Sep 29, 2025, 10:10 AM

--- L09_text_alarm.js
+++ L09_text_alarm.js
@@ -3,7 +3,7 @@ let velocityX = 4;
 let sfx;
 let bgcolor = "pink";
 let objectID;
-let counter = 10;
+let counter = 100;
 function preload() {
     sfx = loadSound("assets/Win.mp3");
 }
@@ -15,7 +15,7 @@ function setup() {
 }
 function countdown() {
     counter--;
-    counter = constrain(counter, 0, 10);
+    counter = constrain(counter, 0, 100);
     if (counter === 0) {
         sfx.play();
         clearInterval(objectID);
