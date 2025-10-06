63b4584fb30262baecd42a60c44af11fd1aae4eb
Elijah2107 <olgawong@hotmail.com>
2025-10-06 10:21:16 +0000

    Oct 6, 2025, 10:21 AM

--- L10_name.js
+++ L10_name.js
@@ -12,6 +12,7 @@ function setup() {
     userInput = createInput(username);
     userInput.position(width/2,height/2 + 50);
     userInput.input(updateName);
+    ageInput.input
 }
 function updateName() {
     username = userInput.value();
