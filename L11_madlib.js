03b7ec697fb5f649ab68b6206e890bd7dd472965
Elijah2107 <olgawong@hotmail.com>
2025-10-13 10:02:02 +0000

    Oct 13, 2025, 10:02 AM

--- L11_madlib.js
+++ L11_madlib.js
@@ -8,7 +8,8 @@ function setup() {
     button1.position(50, 50);
     button1.size(100,80);
     button1.mousePressed( changeText );
-    story = 0;
+    
+    //story = 0;
 }
 function changeText() {
     story++;
