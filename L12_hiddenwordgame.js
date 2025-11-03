5fd584c593dcbf6e3a39d3dbd11999cbae334531
Elijah2107 <olgawong@hotmail.com>
2025-11-03 10:47:37 +0000

    Nov 3, 2025, 10:47 AM

--- L12_hiddenwordgame.js
+++ L12_hiddenwordgame.js
@@ -22,7 +22,7 @@ function setup() {
     generateHints();
 }
 function generateHints() {
-    hints = selected[0] + " " + "_ "
+    hints = selected[0] + " " + "_ ".repeat()
 }
 function checkAnswer() {
     message = "You guessed it! The hidden word is 'beach'";
