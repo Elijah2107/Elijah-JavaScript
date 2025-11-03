e9617148f7b1f0264ad8ee6f63c7b875f491b523
Elijah2107 <olgawong@hotmail.com>
2025-11-03 10:25:29 +0000

    Nov 3, 2025, 10:25 AM

--- L12_hiddenwordgame.js
+++ L12_hiddenwordgame.js
@@ -6,7 +6,7 @@ function setup() {
     background("lightblue");
     guessButton = createButton("Guess");
     guessButton.size(150,30);
-    g
+    guessButton
     guessButton.position(400,250);
     guessInput = createInput();
     guessInput.size(150,30);
